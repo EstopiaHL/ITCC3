f929615575e67f6b9946f5e2a17a173da38b0c60
EstopiaHL <200520218@my.xu.edu.ph>
2023-05-18 02:14:41 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA SIGN IN GOOD

---
+++ SignUP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Laundry palace data entry</title>
+   </head>
+   <body>
+      <center>
+         <h1>Storing Form data in Database</h1>
+         <form action="SignUPEngine.php" method="post">
+<p>
+               <label for="firstName">Username:</label>
+               <input type="text" name="first_name" id="firstName">
+            </p>
+             
+<p>
+               <label for="lastName">Password:</label>
+               <input type="text" name="last_name" id="lastName">
+            </p>
+ 
+            <input type="submit" value="Submit">
+         </form>
+      </center>
+   </body>
+</html>
